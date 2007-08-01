6a1b4bad7745eadc73fdc4fe7552915b15ca6b23
mungady <mungady@fd644a56-f332-0410-b4b8-f002098053bd>
2007-08-01 09:47:04 +0000

    2007-07-31  David Gilbert  <david.gilbert@object-refinery.com>
    	Bug 1763413:
    	* source/org/jfree/chart/axis/PeriodAxis.java
    	(drawTickLabels): Calculate tick label width correctly for inverted axis.

--- source/org/jfree/chart/axis/PeriodAxis.java
+++ source/org/jfree/chart/axis/PeriodAxis.java
@@ -52,6 +52,7 @@
  * 22-Mar-2007 : Use new defaultAutoRange attribute (DG);
  * 20-Jun-2007 : Removed JCommon dependencies (DG);
  * 02-Jul-2007 : Added entity support for axis labels (DG);
+ * 31-Jul-2007 : Fix for inverted axis labelling (see bug 1763413) (DG);
  *
  */
 
@@ -810,7 +811,13 @@ public class PeriodAxis extends ValueAxis
                 g2.getFontMetrics());
         double w = Math.max(b1.getWidth(), b2.getWidth());
         long ww = Math.round(java2DToValue(dataArea.getX() + w + 5.0, 
-                dataArea, edge)) - axisMin;
+                dataArea, edge));
+        if (isInverted()) {
+            ww = axisMax - ww;
+        }
+        else {
+            ww = ww - axisMin;
+        }
         long length = p1.getLastMillisecond(this.calendar) 
                       - p1.getFirstMillisecond(this.calendar);
         int periods = (int) (ww / length) + 1;
