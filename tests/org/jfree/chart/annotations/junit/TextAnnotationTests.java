2b3ca3c4a40c2eec55b2dc8f2ed2c352ce29dbcd
mungady <mungady@fd644a56-f332-0410-b4b8-f002098053bd>
2007-10-11 08:50:44 +0000

    Removed CVS $Id tags.

--- tests/org/jfree/chart/annotations/junit/TextAnnotationTests.java
+++ tests/org/jfree/chart/annotations/junit/TextAnnotationTests.java
@@ -32,8 +32,6 @@
  * Original Author:  David Gilbert (for Object Refinery Limited);
  * Contributor(s):   -;
  *
- * $Id: TextAnnotationTests.java,v 1.1.2.1 2006/10/03 15:41:40 mungady Exp $
- *
  * Changes
  * -------
  * 19-Aug-2003 : Version 1 (DG);
