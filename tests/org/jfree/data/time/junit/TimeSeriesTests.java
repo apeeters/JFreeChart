c005dfb4b2c27d49d42bad60aa5ae6550c355844
mungady <mungady@fd644a56-f332-0410-b4b8-f002098053bd>
2007-10-18 08:21:09 +0000

    Removed CVS $Id tags.

--- tests/org/jfree/data/time/junit/TimeSeriesTests.java
+++ tests/org/jfree/data/time/junit/TimeSeriesTests.java
@@ -32,8 +32,6 @@
  * Original Author:  David Gilbert (for Object Refinery Limited);
  * Contributor(s):   -;
  *
- * $Id: TimeSeriesTests.java,v 1.1.2.1 2006/10/03 15:41:39 mungady Exp $
- *
  * Changes
  * -------
  * 16-Nov-2001 : Version 1 (DG);
