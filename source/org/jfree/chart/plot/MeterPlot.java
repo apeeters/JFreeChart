ac8886b5f9989cc3abf44b10867c674b692424a8
mungady <mungady@fd644a56-f332-0410-b4b8-f002098053bd>
2007-11-29 14:30:49 +0000

    2007-11-29  David Gilbert  <david.gilbert@object-refinery.com>
    	* source/org/jfree/chart/plot/MeterPlot.java
    	(writeObject): Handle dialOutlinePaint,
    	(readObject): Likewise,
    	* tests/org/jfree/chart/plot/junit/MeterPlotTests.java
    	(testEquals): Updated to test dialOutlinePaint,
    	(testSerialization1): Likewise.

--- source/org/jfree/chart/plot/MeterPlot.java
+++ source/org/jfree/chart/plot/MeterPlot.java
@@ -79,6 +79,7 @@
  * 05-Mar-2007 : Restore clip region correctly (see bug 1667750) (DG);
  * 18-May-2007 : Set dataset for LegendItem (DG);
  * 20-Jun-2007 : Removed JCommon dependencies (DG);
+ * 29-Nov-2007 : Fixed serialization bug with dialOutlinePaint (DG);
  * 
  */
 
@@ -1280,6 +1281,7 @@ public class MeterPlot extends Plot implements Serializable, Cloneable {
     private void writeObject(ObjectOutputStream stream) throws IOException {
         stream.defaultWriteObject();
         SerialUtilities.writePaint(this.dialBackgroundPaint, stream);
+        SerialUtilities.writePaint(this.dialOutlinePaint, stream);
         SerialUtilities.writePaint(this.needlePaint, stream);
         SerialUtilities.writePaint(this.valuePaint, stream);
         SerialUtilities.writePaint(this.tickPaint, stream);
@@ -1298,6 +1300,7 @@ public class MeterPlot extends Plot implements Serializable, Cloneable {
         throws IOException, ClassNotFoundException {
         stream.defaultReadObject();
         this.dialBackgroundPaint = SerialUtilities.readPaint(stream);
+        this.dialOutlinePaint = SerialUtilities.readPaint(stream);
         this.needlePaint = SerialUtilities.readPaint(stream);
         this.valuePaint = SerialUtilities.readPaint(stream);
         this.tickPaint = SerialUtilities.readPaint(stream);
