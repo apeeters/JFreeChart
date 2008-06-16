7404ea52e5b3592128e5884de277e5d9c8dbf9d4
mungady <mungady@fd644a56-f332-0410-b4b8-f002098053bd>
2008-06-16 18:42:08 +0000

    2006-06-16  David Gilbert  <david.gilbert@object-refinery.com>
        * source/org/jfree/chart/renderer/xy/YIntervalRenderer.java
        (additionalItemLabelGenerator): New field,
        (YIntervalRenderer): Initialise new field,
        (getAdditionalItemLabelGenerator): New method,
        (setAdditionalItemLabelGenerator): Likewise,
        (drawItem): Added additional item label support,
        (drawAdditionalItemLabel): New method,
        (equals): Include new field.

--- tests/org/jfree/chart/renderer/xy/junit/YIntervalRendererTests.java
+++ tests/org/jfree/chart/renderer/xy/junit/YIntervalRendererTests.java
@@ -37,6 +37,8 @@
  * 25-Mar-2003 : Version 1 (DG);
  * 20-Feb-2007 : Extended the testEquals() checks (DG);
  * 17-May-2007 : Added testGetLegendItemSeriesIndex() (DG);
+ * 22-Apr-2008 : Added testPublicCloneable() (DG);
+ * 26-May-2008 : Extended testEquals() (DG);
  *
  */
 
@@ -57,6 +59,7 @@ import org.jfree.chart.JFreeChart;
 import org.jfree.chart.LegendItem;
 import org.jfree.chart.annotations.XYTextAnnotation;
 import org.jfree.chart.axis.NumberAxis;
+import org.jfree.chart.labels.IntervalXYItemLabelGenerator;
 import org.jfree.chart.labels.StandardXYItemLabelGenerator;
 import org.jfree.chart.labels.StandardXYSeriesLabelGenerator;
 import org.jfree.chart.labels.StandardXYToolTipGenerator;
@@ -64,6 +67,7 @@ import org.jfree.chart.plot.XYPlot;
 import org.jfree.chart.renderer.xy.YIntervalRenderer;
 import org.jfree.chart.urls.StandardXYURLGenerator;
 import org.jfree.chart.util.Layer;
+import org.jfree.chart.util.PublicCloneable;
 import org.jfree.data.xy.YIntervalSeries;
 import org.jfree.data.xy.YIntervalSeriesCollection;
 
@@ -160,6 +164,12 @@ public class YIntervalRendererTests extends TestCase {
         assertFalse(r1.equals(r2));
         r2.setLegendItemURLGenerator(new StandardXYSeriesLabelGenerator());
         assertTrue(r1.equals(r2));
+
+        r1.setAdditionalItemLabelGenerator(new IntervalXYItemLabelGenerator());
+        assertFalse(r1.equals(r2));
+        r2.setAdditionalItemLabelGenerator(new IntervalXYItemLabelGenerator());
+        assertTrue(r1.equals(r2));
+
     }
 
     /**
@@ -215,6 +225,14 @@ public class YIntervalRendererTests extends TestCase {
                 Layer.BACKGROUND);
         assertTrue(r1.equals(r2));
 
+    }
+
+    /**
+     * Verify that this class implements {@link PublicCloneable}.
+     */
+    public void testPublicCloneable() {
+        YIntervalRenderer r1 = new YIntervalRenderer();
+        assertTrue(r1 instanceof PublicCloneable);
     }
 
     /**
