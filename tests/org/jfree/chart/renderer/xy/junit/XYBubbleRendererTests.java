85b433e26652feeb71360f54d1e4281e68212cbe
mungady <mungady@fd644a56-f332-0410-b4b8-f002098053bd>
2008-06-16 18:29:33 +0000

    Added testPublicCloneable.

--- tests/org/jfree/chart/renderer/xy/junit/XYBubbleRendererTests.java
+++ tests/org/jfree/chart/renderer/xy/junit/XYBubbleRendererTests.java
@@ -37,6 +37,7 @@
  * 25-Mar-2003 : Version 1 (DG);
  * 24-Jan-2007 : Added more checks to testEquals() (DG);
  * 17-May-2007 : Added testGetLegendItemSeriesIndex() (DG);
+ * 22-Apr-2008 : Added testPublicCloneable (DG);
  *
  */
 
@@ -58,6 +59,7 @@ import org.jfree.chart.LegendItem;
 import org.jfree.chart.axis.NumberAxis;
 import org.jfree.chart.plot.XYPlot;
 import org.jfree.chart.renderer.xy.XYBubbleRenderer;
+import org.jfree.chart.util.PublicCloneable;
 import org.jfree.data.xy.DefaultXYZDataset;
 
 /**
@@ -127,6 +129,14 @@ public class XYBubbleRendererTests extends TestCase {
     }
 
     /**
+     * Verify that this class implements {@link PublicCloneable}.
+     */
+    public void testPublicCloneable() {
+        XYBubbleRenderer r1 = new XYBubbleRenderer();
+        assertTrue(r1 instanceof PublicCloneable);
+    }
+
+    /**
      * Serialize an instance, restore it, and check for equality.
      */
     public void testSerialization() {
@@ -159,29 +169,29 @@ public class XYBubbleRendererTests extends TestCase {
         double[] x = {2.1, 2.3, 2.3, 2.2, 2.2, 1.8, 1.8, 1.9, 2.3, 3.8};
         double[] y = {14.1, 11.1, 10.0, 8.8, 8.7, 8.4, 5.4, 4.1, 4.1, 25};
         double[] z = {2.4, 2.7, 2.7, 2.2, 2.2, 2.2, 2.1, 2.2, 1.6, 4};
-        double[][] s1 = new double[][] { x, y, z };
+        double[][] s1 = new double[][] {x, y, z};
         d1.addSeries("S1", s1);
         x = new double[] {2.1};
         y = new double[] {14.1};
         z = new double[] {2.4};
-        double[][] s2 = new double[][] { x, y, z };
+        double[][] s2 = new double[][] {x, y, z};
         d1.addSeries("S2", s2);
 
         DefaultXYZDataset d2 = new DefaultXYZDataset();
         x = new double[] {2.1};
         y = new double[] {14.1};
         z = new double[] {2.4};
-        double[][] s3 = new double[][] { x, y, z };
+        double[][] s3 = new double[][] {x, y, z};
         d2.addSeries("S3", s3);
         x = new double[] {2.1};
         y = new double[] {14.1};
         z = new double[] {2.4};
-        double[][] s4 = new double[][] { x, y, z };
+        double[][] s4 = new double[][] {x, y, z};
         d2.addSeries("S4", s4);
         x = new double[] {2.1};
         y = new double[] {14.1};
         z = new double[] {2.4};
-        double[][] s5 = new double[][] { x, y, z };
+        double[][] s5 = new double[][] {x, y, z};
         d2.addSeries("S5", s5);
 
         XYBubbleRenderer r = new XYBubbleRenderer();
