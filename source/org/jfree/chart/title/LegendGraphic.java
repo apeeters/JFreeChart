581c59f51a7dfb52c2bdff068792bcde3728a3ad
mungady <mungady@fd644a56-f332-0410-b4b8-f002098053bd>
2008-06-18 13:45:49 +0000

    Updated for changes in 1.0.x branch.

--- source/org/jfree/chart/title/LegendGraphic.java
+++ source/org/jfree/chart/title/LegendGraphic.java
@@ -82,6 +82,9 @@ import org.jfree.chart.util.StandardGradientPaintTransformer;
 public class LegendGraphic extends AbstractBlock
                            implements Block, PublicCloneable {
 
+    /** For serialization. */
+    static final long serialVersionUID = -1338791523854985009L;
+
     /**
      * A flag that controls whether or not the shape is visible - see also
      * lineVisible.
