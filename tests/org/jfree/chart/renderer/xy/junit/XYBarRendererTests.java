4e1acd3ef7bd1a22f428ffbbe3256b12543d28d3
mungady <mungady@fd644a56-f332-0410-b4b8-f002098053bd>
2008-06-19 13:48:51 +0000

    2008-06-19  David Gilbert  <david.gilbert@object-refinery.com>
        * source/org/jfree/chart/renderer/xy/XYBarRenderer.java
        (findRangeBounds): New method override,
        * tests/org/jfree/chart/renderer/xy/junit/XYBarRendererTests.java
        (testFindRangeBounds): New test method.

--- tests/org/jfree/chart/renderer/xy/junit/XYBarRendererTests.java
+++ tests/org/jfree/chart/renderer/xy/junit/XYBarRendererTests.java
@@ -40,6 +40,7 @@
  * 17-May-2007 : Added testGetLegendItemSeriesIndex() (DG);
  * 20-Jun-2007 : Removed JCommon dependencies (DG);
  * 22-Apr-2008 : Added testPublicCloneable (DG);
+ * 19-Jun-2008 : Added testFindRangeBounds() (DG);
  *
  */
 
@@ -69,6 +70,7 @@ import org.jfree.chart.util.GradientPaintTransformType;
 import org.jfree.chart.util.PublicCloneable;
 import org.jfree.chart.util.StandardGradientPaintTransformer;
 import org.jfree.data.Range;
+import org.jfree.data.xy.DefaultIntervalXYDataset;
 import org.jfree.data.xy.XYBarDataset;
 import org.jfree.data.xy.XYSeries;
 import org.jfree.data.xy.XYSeriesCollection;
@@ -273,6 +275,33 @@ public class XYBarRendererTests extends TestCase {
         assertTrue(bounds.contains(0.5));
         assertTrue(bounds.contains(2.5));
         assertFalse(bounds.contains(2.8));
+    }
+
+    private static final double EPSILON = 0.0000000001;
+
+    /**
+     * A simple test for the findRangeBounds() method.
+     */
+    public void testFindRangeBounds() {
+        DefaultIntervalXYDataset dataset = new DefaultIntervalXYDataset();
+        double[] x = {1.0, 2.0, 3.0, 4.0};
+        double[] startx = {0.9, 1.8, 2.7, 3.6};
+        double[] endx = {1.1, 2.2, 3.3, 4.4};
+        double[] y = {1.0, 2.0, 3.0, 4.0};
+        double[] starty = {0.9, 1.8, 2.7, 3.6};
+        double[] endy = {1.1, 2.2, 3.3, 4.4};
+        double[][] data = new double[][] {x, startx, endx, y, starty, endy};
+        dataset.addSeries("Series 1", data);
+        XYBarRenderer renderer = new XYBarRenderer();
+        renderer.setUseYInterval(true);
+        Range r = renderer.findRangeBounds(dataset);
+        assertEquals(0.9, r.getLowerBound(), EPSILON);
+        assertEquals(4.4, r.getUpperBound(), EPSILON);
+
+        renderer.setUseYInterval(false);
+        r = renderer.findRangeBounds(dataset);
+        assertEquals(1.0, r.getLowerBound(), EPSILON);
+        assertEquals(4.0, r.getUpperBound(), EPSILON);
     }
 
     /**
