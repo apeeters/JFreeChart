3cef0709cb55be589753f854119083b4718b7394
mungady <mungady@fd644a56-f332-0410-b4b8-f002098053bd>
2008-07-22 14:59:57 +0000

    Whitespace.

--- source/org/jfree/data/gantt/GanttCategoryDataset.java
+++ source/org/jfree/data/gantt/GanttCategoryDataset.java
@@ -2,32 +2,32 @@
  * JFreeChart : a free chart library for the Java(tm) platform
  * ===========================================================
  *
- * (C) Copyright 2000-2007, by Object Refinery Limited and Contributors.
+ * (C) Copyright 2000-2008, by Object Refinery Limited and Contributors.
  *
  * Project Info:  http://www.jfree.org/jfreechart/index.html
  *
- * This library is free software; you can redistribute it and/or modify it 
- * under the terms of the GNU Lesser General Public License as published by 
- * the Free Software Foundation; either version 2.1 of the License, or 
+ * This library is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation; either version 2.1 of the License, or
  * (at your option) any later version.
  *
- * This library is distributed in the hope that it will be useful, but 
- * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY 
- * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public 
+ * This library is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public
  * License for more details.
  *
  * You should have received a copy of the GNU Lesser General Public
  * License along with this library; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, 
- * USA.  
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301,
+ * USA.
  *
- * [Java is a trademark or registered trademark of Sun Microsystems, Inc. 
+ * [Java is a trademark or registered trademark of Sun Microsystems, Inc.
  * in the United States and other countries.]
  *
  * -------------------------
  * GanttCategoryDataset.java
  * -------------------------
- * (C) Copyright 2003-2007, by Object Refinery Limited.
+ * (C) Copyright 2003-2008, by Object Refinery Limited.
  *
  * Original Author:  David Gilbert (for Object Refinery Limited);
  * Contributor(s):   -;
@@ -44,7 +44,7 @@
 import org.jfree.data.category.IntervalCategoryDataset;
 
 /**
- * An extension of the {@link IntervalCategoryDataset} interface that adds 
+ * An extension of the {@link IntervalCategoryDataset} interface that adds
  * support for multiple sub-intervals.
  */
 public interface GanttCategoryDataset extends IntervalCategoryDataset {
@@ -109,7 +109,7 @@ public interface GanttCategoryDataset extends IntervalCategoryDataset {
      *
      * @return The start value (possibly <code>null</code>).
      */
-    public Number getStartValue(Comparable rowKey, Comparable columnKey, 
+    public Number getStartValue(Comparable rowKey, Comparable columnKey,
                                 int subinterval);
 
     /**
@@ -132,7 +132,7 @@ public interface GanttCategoryDataset extends IntervalCategoryDataset {
      *
      * @return The end value (possibly <code>null</code>).
      */
-    public Number getEndValue(Comparable rowKey, Comparable columnKey, 
+    public Number getEndValue(Comparable rowKey, Comparable columnKey,
                               int subinterval);
 
     /**
@@ -155,7 +155,7 @@ public interface GanttCategoryDataset extends IntervalCategoryDataset {
      *
      * @return The precent complete value (possibly <code>null</code>).
      */
-    public Number getPercentComplete(Comparable rowKey, Comparable columnKey, 
+    public Number getPercentComplete(Comparable rowKey, Comparable columnKey,
                                      int subinterval);
 
 }
--- source/org/jfree/data/gantt/TaskSeries.java
+++ source/org/jfree/data/gantt/TaskSeries.java
@@ -2,32 +2,32 @@
  * JFreeChart : a free chart library for the Java(tm) platform
  * ===========================================================
  *
- * (C) Copyright 2000-2007, by Object Refinery Limited and Contributors.
+ * (C) Copyright 2000-2008, by Object Refinery Limited and Contributors.
  *
  * Project Info:  http://www.jfree.org/jfreechart/index.html
  *
- * This library is free software; you can redistribute it and/or modify it 
- * under the terms of the GNU Lesser General Public License as published by 
- * the Free Software Foundation; either version 2.1 of the License, or 
+ * This library is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation; either version 2.1 of the License, or
  * (at your option) any later version.
  *
- * This library is distributed in the hope that it will be useful, but 
- * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY 
- * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public 
+ * This library is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public
  * License for more details.
  *
  * You should have received a copy of the GNU Lesser General Public
  * License along with this library; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, 
- * USA.  
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301,
+ * USA.
  *
- * [Java is a trademark or registered trademark of Sun Microsystems, Inc. 
+ * [Java is a trademark or registered trademark of Sun Microsystems, Inc.
  * in the United States and other countries.]
  *
  * ---------------
  * TaskSeries.java
  * ---------------
- * (C) Copyright 2002-2007, by Object Refinery Limited.
+ * (C) Copyright 2002-2008, by Object Refinery Limited.
  *
  * Original Author:  David Gilbert (for Object Refinery Limited);
  * Contributor(s):   -;
@@ -71,8 +71,8 @@ public class TaskSeries extends Series {
     }
 
     /**
-     * Adds a task to the series and sends a 
-     * {@link org.jfree.data.general.SeriesChangeEvent} to all registered 
+     * Adds a task to the series and sends a
+     * {@link org.jfree.data.general.SeriesChangeEvent} to all registered
      * listeners.
      *
      * @param task  the task (<code>null</code> not permitted).
@@ -86,8 +86,8 @@ public class TaskSeries extends Series {
     }
 
     /**
-     * Removes a task from the series and sends 
-     * a {@link org.jfree.data.general.SeriesChangeEvent} 
+     * Removes a task from the series and sends
+     * a {@link org.jfree.data.general.SeriesChangeEvent}
      * to all registered listeners.
      *
      * @param task  the task.
@@ -98,8 +98,8 @@ public class TaskSeries extends Series {
     }
 
     /**
-     * Removes all tasks from the series and sends 
-     * a {@link org.jfree.data.general.SeriesChangeEvent} 
+     * Removes all tasks from the series and sends
+     * a {@link org.jfree.data.general.SeriesChangeEvent}
      * to all registered listeners.
      */
     public void removeAll() {
@@ -126,12 +126,12 @@ public class TaskSeries extends Series {
     public Task get(int index) {
         return (Task) this.tasks.get(index);
     }
-    
+
     /**
      * Returns the task in the series that has the specified description.
-     * 
+     *
      * @param description  the name (<code>null</code> not permitted).
-     * 
+     *
      * @return The task (possibly <code>null</code>).
      */
     public Task get(String description) {
@@ -158,9 +158,9 @@ public class TaskSeries extends Series {
 
     /**
      * Tests this object for equality with an arbitrary object.
-     * 
+     *
      * @param obj  the object to test against (<code>null</code> permitted).
-     * 
+     *
      * @return A boolean.
      */
     public boolean equals(Object obj) {
@@ -179,5 +179,5 @@ public class TaskSeries extends Series {
         }
         return true;
     }
-    
+
 }
