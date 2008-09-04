c04a483d6e478f43fdbab6e4c91b5c6e297df530
mungady <mungady@fd644a56-f332-0410-b4b8-f002098053bd>
2008-09-04 20:38:50 +0000

    Synchronised with 1.0.x branch.

--- tests/org/jfree/data/xy/junit/DefaultOHLCDatasetTests.java
+++ tests/org/jfree/data/xy/junit/DefaultOHLCDatasetTests.java
@@ -2,32 +2,32 @@
  * JFreeChart : a free chart library for the Java(tm) platform
  * ===========================================================
  *
- * (C) Copyright 2000-2007, by Object Refinery Limited and Contributors.
+ * (C) Copyright 2000-2008, by Object Refinery Limited and Contributors.
  *
  * Project Info:  http://www.jfree.org/jfreechart/index.html
  *
- * This library is free software; you can redistribute it and/or modify it 
- * under the terms of the GNU Lesser General Public License as published by 
- * the Free Software Foundation; either version 2.1 of the License, or 
+ * This library is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation; either version 2.1 of the License, or
  * (at your option) any later version.
  *
- * This library is distributed in the hope that it will be useful, but 
- * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY 
- * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public 
+ * This library is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public
  * License for more details.
  *
  * You should have received a copy of the GNU Lesser General Public
  * License along with this library; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, 
- * USA.  
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301,
+ * USA.
  *
- * [Java is a trademark or registered trademark of Sun Microsystems, Inc. 
+ * [Java is a trademark or registered trademark of Sun Microsystems, Inc.
  * in the United States and other countries.]
  *
  * ----------------------------
  * DefaultOHLCDatasetTests.java
  * ----------------------------
- * (C) Copyright 2005-2007, by Object Refinery Limited and Contributors.
+ * (C) Copyright 2005-2008, by Object Refinery Limited and Contributors.
  *
  * Original Author:  David Gilbert (for Object Refinery Limited);
  * Contributor(s):   -;
@@ -36,6 +36,7 @@
  * -------
  * 29-Apr-2005 : Version 1 (DG);
  * 28-Nov-2006 : Extended equals() test (DG);
+ * 22-Apr-2008 : Added testPublicCloneable (DG);
  *
  */
 
@@ -53,6 +54,7 @@ import junit.framework.Test;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
 
+import org.jfree.chart.util.PublicCloneable;
 import org.jfree.data.Range;
 import org.jfree.data.general.DatasetUtilities;
 import org.jfree.data.xy.DefaultOHLCDataset;
@@ -82,7 +84,7 @@ public class DefaultOHLCDatasetTests extends TestCase {
     }
 
     private static final double EPSILON = 0.0000000001;
-    
+
     /**
      * A small test for the data range calculated on this dataset.
      */
@@ -92,42 +94,41 @@ public class DefaultOHLCDatasetTests extends TestCase {
         data[1] = new OHLCDataItem(new Date(22L), 4.0, 9.0, 2.0, 5.0, 120.0);
         data[2] = new OHLCDataItem(new Date(33L), 3.0, 7.0, 3.0, 6.0, 140.0);
         DefaultOHLCDataset d = new DefaultOHLCDataset("S1", data);
-        Range r = DatasetUtilities.findRangeBounds(d, false);
+        Range r = DatasetUtilities.findRangeBounds(d, true);
         assertEquals(1.0, r.getLowerBound(), EPSILON);
         assertEquals(9.0, r.getUpperBound(), EPSILON);
     }
-    
+
     /**
      * Confirm that the equals method can distinguish all the required fields.
      */
     public void testEquals() {
-        DefaultOHLCDataset d1 = new DefaultOHLCDataset("Series 1", 
+        DefaultOHLCDataset d1 = new DefaultOHLCDataset("Series 1",
                 new OHLCDataItem[0]);
-        DefaultOHLCDataset d2 = new DefaultOHLCDataset("Series 1", 
+        DefaultOHLCDataset d2 = new DefaultOHLCDataset("Series 1",
                 new OHLCDataItem[0]);
         assertTrue(d1.equals(d2));
         assertTrue(d2.equals(d1));
-        
+
         d1 = new DefaultOHLCDataset("Series 2", new OHLCDataItem[0]);
         assertFalse(d1.equals(d2));
         d2 = new DefaultOHLCDataset("Series 2", new OHLCDataItem[0]);
         assertTrue(d1.equals(d2));
-        
+
         d1 = new DefaultOHLCDataset("Series 2", new OHLCDataItem[] {
                 new OHLCDataItem(new Date(123L), 1.2, 3.4, 5.6, 7.8, 99.9)});
         assertFalse(d1.equals(d2));
         d2 = new DefaultOHLCDataset("Series 2", new OHLCDataItem[] {
                 new OHLCDataItem(new Date(123L), 1.2, 3.4, 5.6, 7.8, 99.9)});
         assertTrue(d1.equals(d2));
-        
-        
+
     }
 
     /**
      * Confirm that cloning works.
      */
     public void testCloning() {
-        DefaultOHLCDataset d1 = new DefaultOHLCDataset("Series 1", 
+        DefaultOHLCDataset d1 = new DefaultOHLCDataset("Series 1",
                 new OHLCDataItem[0]);
         DefaultOHLCDataset d2 = null;
         try {
@@ -142,10 +143,45 @@ public class DefaultOHLCDatasetTests extends TestCase {
     }
 
     /**
+     * Confirm that cloning works.
+     */
+    public void testCloning2() {
+        OHLCDataItem item1 = new OHLCDataItem(new Date(1L), 1.0, 2.0, 3.0, 4.0,
+                5.0);
+        OHLCDataItem item2 = new OHLCDataItem(new Date(2L), 6.0, 7.0, 8.0, 9.0,
+                10.0);
+        // create an array of items in reverse order
+        OHLCDataItem[] items = new OHLCDataItem[] {item2, item1};
+        DefaultOHLCDataset d1 = new DefaultOHLCDataset("Series 1", items);
+        DefaultOHLCDataset d2 = null;
+        try {
+            d2 = (DefaultOHLCDataset) d1.clone();
+        }
+        catch (CloneNotSupportedException e) {
+            e.printStackTrace();
+        }
+        assertTrue(d1 != d2);
+        assertTrue(d1.getClass() == d2.getClass());
+        assertTrue(d1.equals(d2));
+
+        d1.sortDataByDate();
+        assertFalse(d1.equals(d2));
+    }
+
+    /**
+     * Verify that this class implements {@link PublicCloneable}.
+     */
+    public void testPublicCloneable() {
+        DefaultOHLCDataset d1 = new DefaultOHLCDataset("Series 1",
+                new OHLCDataItem[0]);
+        assertTrue(d1 instanceof PublicCloneable);
+    }
+
+    /**
      * Serialize an instance, restore it, and check for equality.
      */
     public void testSerialization() {
-        DefaultOHLCDataset d1 = new DefaultOHLCDataset("Series 1", 
+        DefaultOHLCDataset d1 = new DefaultOHLCDataset("Series 1",
                 new OHLCDataItem[0]);
         DefaultOHLCDataset d2 = null;
 
