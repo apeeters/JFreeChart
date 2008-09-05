959a444f0eaf45ca286448f81778f30f607bdf52
mungady <mungady@fd644a56-f332-0410-b4b8-f002098053bd>
2008-09-05 06:48:36 +0000

    Whitespace.

--- experimental/org/jfree/experimental/chart/util/XYCoordinateType.java
+++ experimental/org/jfree/experimental/chart/util/XYCoordinateType.java
@@ -2,32 +2,32 @@
  * JFreeChart : a free chart library for the Java(tm) platform
  * ===========================================================
  *
- * (C) Copyright 2000-2007, by Object Refinery Limited and Contributors.
+ * (C) Copyright 2000-2008, by Object Refinery Limited and Contributors.
  *
  * Project Info:  http://www.jfree.org/jfreechart/index.html
  *
- * This library is free software; you can redistribute it and/or modify it 
- * under the terms of the GNU Lesser General Public License as published by 
- * the Free Software Foundation; either version 2.1 of the License, or 
+ * This library is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation; either version 2.1 of the License, or
  * (at your option) any later version.
  *
- * This library is distributed in the hope that it will be useful, but 
- * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY 
- * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public 
+ * This library is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public
  * License for more details.
  *
  * You should have received a copy of the GNU Lesser General Public
  * License along with this library; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, 
- * USA.  
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301,
+ * USA.
  *
- * [Java is a trademark or registered trademark of Sun Microsystems, Inc. 
+ * [Java is a trademark or registered trademark of Sun Microsystems, Inc.
  * in the United States and other countries.]
  *
  * ---------------------
  * XYCoordinateType.java
  * ---------------------
- * (C) Copyright 2007, by Object Refinery Limited and Contributors.
+ * (C) Copyright 2007, 2008, by Object Refinery Limited and Contributors.
  *
  * Original Author:  David Gilbert (for Object Refinery Limited);
  * Contributor(s):   -;
@@ -35,7 +35,7 @@
  * Changes
  * -------
  * 02-Feb-2007 : Version 1 (DG);
- * 
+ *
  */
 
 package org.jfree.experimental.chart.util;
@@ -47,23 +47,23 @@ import java.io.Serializable;
  * Represents several possible interpretations for an (x, y) coordinate.
  */
 public final class XYCoordinateType implements Serializable {
-    
+
     /** The (x, y) coordinates represent a point in the data space. */
-    public static final XYCoordinateType DATA 
+    public static final XYCoordinateType DATA
             = new XYCoordinateType("XYCoordinateType.DATA");
 
-    /** 
-     * The (x, y) coordinates represent a relative position in the data space. 
+    /**
+     * The (x, y) coordinates represent a relative position in the data space.
      * In this case, the values should be in the range (0.0 to 1.0).
      */
-    public static final XYCoordinateType RELATIVE 
+    public static final XYCoordinateType RELATIVE
             = new XYCoordinateType("XYCoordinateType.RELATIVE");
 
-    /** 
-     * The (x, y) coordinates represent indices in a dataset. 
+    /**
+     * The (x, y) coordinates represent indices in a dataset.
      * In this case, the values should be in the range (0.0 to 1.0).
      */
-    public static final XYCoordinateType INDEX 
+    public static final XYCoordinateType INDEX
             = new XYCoordinateType("XYCoordinateType.INDEX");
 
     /** The name. */
@@ -88,7 +88,7 @@ public final class XYCoordinateType implements Serializable {
     }
 
     /**
-     * Returns <code>true</code> if this object is equal to the specified 
+     * Returns <code>true</code> if this object is equal to the specified
      * object, and <code>false</code> otherwise.
      *
      * @param obj  the other object.
@@ -108,7 +108,7 @@ public final class XYCoordinateType implements Serializable {
         }
         return true;
     }
-   
+
     /**
      * Ensures that serialization returns the unique instances.
      *
@@ -122,10 +122,10 @@ public final class XYCoordinateType implements Serializable {
         }
         else if (this.equals(XYCoordinateType.RELATIVE)) {
             return XYCoordinateType.RELATIVE;
-        }     
+        }
         else if (this.equals(XYCoordinateType.INDEX)) {
             return XYCoordinateType.INDEX;
-        }     
+        }
         return null;
     }
 
