fe2ddd6ae11f5afe869cecc24826b38839e5f2da
mungady <mungady@fd644a56-f332-0410-b4b8-f002098053bd>
2009-04-07 08:25:10 +0000

    Synchronised with 1.0.x branch.

--- source/org/jfree/chart/renderer/LookupPaintScale.java
+++ source/org/jfree/chart/renderer/LookupPaintScale.java
@@ -2,7 +2,7 @@
  * JFreeChart : a free chart library for the Java(tm) platform
  * ===========================================================
  *
- * (C) Copyright 2000-2008, by Object Refinery Limited and Contributors.
+ * (C) Copyright 2000-2009, by Object Refinery Limited and Contributors.
  *
  * Project Info:  http://www.jfree.org/jfreechart/index.html
  *
@@ -27,7 +27,7 @@
  * ---------------------
  * LookupPaintScale.java
  * ---------------------
- * (C) Copyright 2006-2008, by Object Refinery Limited.
+ * (C) Copyright 2006-2009, by Object Refinery Limited.
  *
  * Original Author:  David Gilbert (for Object Refinery Limited);
  * Contributor(s):   -;
@@ -39,6 +39,7 @@
  * 09-Mar-2007 : Fixed cloning (DG);
  * 14-Jun-2007 : Use double primitive in PaintItem (DG);
  * 20-Jun-2007 : Removed deprecated code and JCommon dependencies (DG);
+ * 28-Mar-2009 : Made PaintItem inner class static (DG);
  *
  */
 
@@ -69,7 +70,7 @@ public class LookupPaintScale
     /**
      * Stores the paint for a value.
      */
-    class PaintItem implements Comparable, Serializable {
+    static class PaintItem implements Comparable, Serializable {
 
         /** For serialization. */
         static final long serialVersionUID = 698920578512361570L;
