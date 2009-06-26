bbd155e149b5037b469aaef477ec8379aeb000ff
mungady <mungady@fd644a56-f332-0410-b4b8-f002098053bd>
2009-06-26 14:10:00 +0000

    Synchronised with 1.0.x branch.

--- tests/org/jfree/data/time/ohlc/junit/OHLCSeriesCollectionTests.java
+++ tests/org/jfree/data/time/ohlc/junit/OHLCSeriesCollectionTests.java
@@ -186,6 +186,24 @@ public class OHLCSeriesCollectionTests extends TestCase
     }
 
     /**
+     * Two objects that are equal are required to return the same hashCode.
+     */
+    public void testHashcode() {
+        OHLCSeriesCollection c1 = new OHLCSeriesCollection();
+        OHLCSeries s1 = new OHLCSeries("S");
+        s1.add(new Year(2009), 1.0, 4.0, 0.5, 2.0);
+        c1.addSeries(s1);
+        OHLCSeriesCollection c2 = new OHLCSeriesCollection();
+        OHLCSeries s2 = new OHLCSeries("S");
+        s2.add(new Year(2009), 1.0, 4.0, 0.5, 2.0);
+        c2.addSeries(s2);
+        assertTrue(c1.equals(c2));
+        int h1 = c1.hashCode();
+        int h2 = c2.hashCode();
+        assertEquals(h1, h2);
+    }
+
+    /**
      * Some checks for the {@link OHLCSeriesCollection#removeSeries(int)}
      * method.
      */
