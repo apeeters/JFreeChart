35f95a227274baeb77bf967334cc7c210b78f1f2
mungady <mungady@fd644a56-f332-0410-b4b8-f002098053bd>
2009-07-06 20:15:10 +0000

    Clear off screen buffer to fully transparent.

--- source/org/jfree/chart/ChartPanel.java
+++ source/org/jfree/chart/ChartPanel.java
@@ -164,14 +164,17 @@
  * 08-Jun-2009 : Fixed bug in setMouseWheelEnabled() (DG);
  * 29-Jun-2009 : Implemented new mouse handler mechanism (DG);
  * 29-Jun-2009 : Removed tracelines - use Overlays instead (DG);
+ * 06-Jul-2009 : Clear off-screen buffer to fully transparent (DG);
  * 
  */
 
 package org.jfree.chart;
 
 import java.awt.AWTEvent;
+import java.awt.AlphaComposite;
 import java.awt.BasicStroke;
 import java.awt.Color;
+import java.awt.Composite;
 import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
@@ -1665,12 +1668,17 @@ public class ChartPanel extends JPanel implements ChartChangeListener,
                 Rectangle2D bufferArea = new Rectangle2D.Double(
                         0, 0, this.chartBufferWidth, this.chartBufferHeight);
 
+                // make the background of the buffer clear and transparent
                 Graphics2D bufferG2 = (Graphics2D)
                         this.chartBuffer.getGraphics();
+                Composite savedComposite = bufferG2.getComposite();
+                bufferG2.setComposite(AlphaComposite.getInstance(
+                        AlphaComposite.CLEAR, 0.0f));
                 Rectangle r = new Rectangle(0, 0, this.chartBufferWidth,
                         this.chartBufferHeight);
-                bufferG2.setPaint(getBackground());
                 bufferG2.fill(r);
+                bufferG2.setComposite(savedComposite);
+
                 if (scale) {
                     AffineTransform saved = bufferG2.getTransform();
                     AffineTransform st = AffineTransform.getScaleInstance(
